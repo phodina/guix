3425cb43e7e1b5dffee7493a268d340c483961d7
Vagrant Cascadian <vagrant@debian.org>
2020-08-06 11:43:42 -0700

    gnu: Add %pinebook-pro-patches for linux-libre 5.8.
    * gnu/packages/linux (%pinebook-pro-patches): New variable.
      (linux-libre-5.8-source): Use %pinebook-pro-patches.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -479,12 +479,45 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
     (sha256
      (base32 "1ifnfhpakzffn4b8n7x7w5cps9mzjxlkcfz9zqak2vaw8nzvl39f"))))
 
+(define %pinebook-pro-patches
+  ;; Pinebook Pro patches from manjaro 5.8-rc1 branch:
+  ;; https://gitlab.manjaro.org/tsys/linux-pinebook-pro.git
+  ;; branch v5.8-rc1
+  (search-patches
+   "pinebook-pro-01-leds-Add-support-for-inverted-LED-triggers.patch"
+   "pinebook-pro-02-soc-rockchip-Add-rockchip-suspend-mode-driver.patch"
+   "pinebook-pro-03-firmware-Add-Rockchip-SIP-driver.patch"
+   "pinebook-pro-04-tty-serdev-support-shutdown-op.patch"
+   "pinebook-pro-05-bluetooth-hci_serdev-Clear-registered-bit-on-unregis.patch"
+   "pinebook-pro-06-bluetooth-hci_bcm-disable-power-on-shutdown.patch"
+   "pinebook-pro-07-mmc-core-pwrseq_simple-disable-mmc-power-on-shutdown.patch"
+   "pinebook-pro-08-regulator-core-add-generic-suspend-states-support.patch"
+   "pinebook-pro-09-usb-typec-bus-Catch-crash-due-to-partner-NULL-value.patch"
+   "pinebook-pro-10-usb-typec-tcpm-add-hacky-generic-altmode-support.patch"
+   "pinebook-pro-11-phy-rockchip-typec-Set-extcon-capabilities.patch"
+   "pinebook-pro-12-usb-typec-altmodes-displayport-Add-hacky-generic-alt.patch"
+   "pinebook-pro-13-sound-soc-codecs-es8316-Run-micdetect-only-if-jack-s.patch"
+   "pinebook-pro-14-ASoC-soc-jack.c-supported-inverted-jack-detect-GPIOs.patch"
+   "pinebook-pro-15-arm64-dts-rockchip-add-default-rk3399-rockchip-suspe.patch"
+   "pinebook-pro-16-arm64-dts-rockchip-enable-earlycon.patch"
+   "pinebook-pro-17-arm64-dts-rockchip-reserve-memory-for-ATF-rockchip-S.patch"
+   "pinebook-pro-18-arm64-dts-rockchip-add-cw2015-fuel-gauge.patch"
+   "pinebook-pro-19-arm64-dts-rockchip-use-power-led-for-disk-activity-i.patch"
+   "pinebook-pro-20-arm64-dts-rockchip-add-oficially-unsupported-2GHz-op.patch"
+   "pinebook-pro-21-arm64-dts-rockchip-add-typec-extcon-hack.patch"
+   "pinebook-pro-22-arm64-dts-rockchip-add-rockchip-suspend-node.patch"
+   "pinebook-pro-23-arm64-configs-add-defconfig-for-Pinebook-Pro.patch"
+   "pinebook-pro-24-arm64-dts-rockchip-setup-USB-type-c-port-as-dual-dat.patch"
+   "pinebook-pro-25-arm64-configs-Update-Pinbook-Pro-defconfig-to-v5.8-r.patch"
+   "pinebook-pro-26-soc-rockchip-Port-rockchip_pm_config-driver-to-Linux.patch"))
+
 (define (source-with-patches source patches)
   (origin
     (inherit source)
     (patches (append (origin-patches source)
                      patches))))
 
+<<<<<<< HEAD
 (define-public linux-libre-5.14-source
   (source-with-patches linux-libre-5.14-pristine-source
                        (list %boot-logo-patch
@@ -549,6 +582,13 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
                              ;; Pinebook Pro patch to fix LCD display
                              (search-patch
                               "linux-libre-arm64-generic-pinebook-lcd.patch"))))
+
+(define-public linux-libre-5.8-source
+  (source-with-patches linux-libre-5.8-pristine-source
+                       (append
+			%pinebook-pro-patches
+			(list %boot-logo-patch
+			      %linux-libre-arm-export-__sync_icache_dcache-patch))))
 
 (define-public linux-libre-5.4-source
   (source-with-patches linux-libre-5.4-pristine-source
@@ -1265,6 +1305,14 @@ It has been modified to remove all non-free binary blobs.")
                      #:defconfig "pinebook_pro_defconfig"
                      #:extra-version "pinebook-pro"))
 
+=======
+(define-public linux-libre-pinebook-pro-5.8
+  (make-linux-libre* linux-libre-5.8-version
+                     linux-libre-5.8-source
+                     '("aarch64-linux")
+                     #:defconfig "pinebook_pro_defconfig"
+                     #:extra-version "pinebook-pro"))
+>>>>>>> 47a5442aa7 (gnu: Add %pinebook-pro-patches for linux-libre 5.8.)
 
 ;;;
 ;;; Linux kernel modules.
