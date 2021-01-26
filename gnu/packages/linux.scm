6d31bc7e3fcb57bb62b52092f357b163ec63a496
Vagrant Cascadian <vagrant@debian.org>
2021-01-25 16:49:12 -0800

    gnu: linux-libre-pinebook-pro: Update to 5.10.
    * gnu/packages/linux (linux-libre-pinebook-pro): Update to 5.10.
      (%pinebook-pro-patches): Update to 5.10.
    * gnu/packages/patches: Update patches.
    * gnu/local.mk: Update patches.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -458,36 +458,34 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
      (base32 "1ifnfhpakzffn4b8n7x7w5cps9mzjxlkcfz9zqak2vaw8nzvl39f"))))
 
 (define %pinebook-pro-patches
-  ;; Pinebook Pro patches from manjaro 5.8-rc1 branch:
+  ;; Pinebook Pro patches from manjaro v5.10 branch:
   ;; https://gitlab.manjaro.org/tsys/linux-pinebook-pro.git
-  ;; branch v5.8-rc1
+  ;; Rebased for 5.10.9
   (search-patches
-   "pinebook-pro-01-leds-Add-support-for-inverted-LED-triggers.patch"
-   "pinebook-pro-02-soc-rockchip-Add-rockchip-suspend-mode-driver.patch"
-   "pinebook-pro-03-firmware-Add-Rockchip-SIP-driver.patch"
-   "pinebook-pro-04-tty-serdev-support-shutdown-op.patch"
-   "pinebook-pro-05-bluetooth-hci_serdev-Clear-registered-bit-on-unregis.patch"
-   "pinebook-pro-06-bluetooth-hci_bcm-disable-power-on-shutdown.patch"
-   "pinebook-pro-07-mmc-core-pwrseq_simple-disable-mmc-power-on-shutdown.patch"
-   "pinebook-pro-08-regulator-core-add-generic-suspend-states-support.patch"
-   "pinebook-pro-09-usb-typec-bus-Catch-crash-due-to-partner-NULL-value.patch"
-   "pinebook-pro-10-usb-typec-tcpm-add-hacky-generic-altmode-support.patch"
-   "pinebook-pro-11-phy-rockchip-typec-Set-extcon-capabilities.patch"
-   "pinebook-pro-12-usb-typec-altmodes-displayport-Add-hacky-generic-alt.patch"
-   "pinebook-pro-13-sound-soc-codecs-es8316-Run-micdetect-only-if-jack-s.patch"
-   "pinebook-pro-14-ASoC-soc-jack.c-supported-inverted-jack-detect-GPIOs.patch"
-   "pinebook-pro-15-arm64-dts-rockchip-add-default-rk3399-rockchip-suspe.patch"
-   "pinebook-pro-16-arm64-dts-rockchip-enable-earlycon.patch"
-   "pinebook-pro-17-arm64-dts-rockchip-reserve-memory-for-ATF-rockchip-S.patch"
-   "pinebook-pro-18-arm64-dts-rockchip-add-cw2015-fuel-gauge.patch"
-   "pinebook-pro-19-arm64-dts-rockchip-use-power-led-for-disk-activity-i.patch"
-   "pinebook-pro-20-arm64-dts-rockchip-add-oficially-unsupported-2GHz-op.patch"
-   "pinebook-pro-21-arm64-dts-rockchip-add-typec-extcon-hack.patch"
-   "pinebook-pro-22-arm64-dts-rockchip-add-rockchip-suspend-node.patch"
-   "pinebook-pro-23-arm64-configs-add-defconfig-for-Pinebook-Pro.patch"
-   "pinebook-pro-24-arm64-dts-rockchip-setup-USB-type-c-port-as-dual-dat.patch"
-   "pinebook-pro-25-arm64-configs-Update-Pinbook-Pro-defconfig-to-v5.8-r.patch"
-   "pinebook-pro-26-soc-rockchip-Port-rockchip_pm_config-driver-to-Linux.patch"))
+   "linux-libre-pinebook-pro-01-leds-Add-support-for-inverted-LED-triggers.patch"
+   "linux-libre-pinebook-pro-02-soc-rockchip-Add-rockchip-suspend-mode-driver.patch"
+   "linux-libre-pinebook-pro-03-firmware-Add-Rockchip-SIP-driver.patch"
+   "linux-libre-pinebook-pro-04-tty-serdev-support-shutdown-op.patch"
+   "linux-libre-pinebook-pro-05-bluetooth-hci_serdev-Clear-registered-bit-on-unregis.patch"
+   "linux-libre-pinebook-pro-06-bluetooth-hci_bcm-disable-power-on-shutdown.patch"
+   "linux-libre-pinebook-pro-07-mmc-core-pwrseq_simple-disable-mmc-power-on-shutdown.patch"
+   "linux-libre-pinebook-pro-08-regulator-core-add-generic-suspend-states-support.patch"
+   "linux-libre-pinebook-pro-09-usb-typec-bus-Catch-crash-due-to-partner-NULL-value.patch"
+   "linux-libre-pinebook-pro-10-usb-typec-tcpm-add-hacky-generic-altmode-support.patch"
+   "linux-libre-pinebook-pro-11-phy-rockchip-typec-Set-extcon-capabilities.patch"
+   "linux-libre-pinebook-pro-12-usb-typec-altmodes-displayport-Add-hacky-generic-alt.patch"
+   "linux-libre-pinebook-pro-13-sound-soc-codecs-es8316-Run-micdetect-only-if-jack-s.patch"
+   "linux-libre-pinebook-pro-14-ASoC-soc-jack.c-supported-inverted-jack-detect-GPIOs.patch"
+   "linux-libre-pinebook-pro-15-arm64-dts-rockchip-add-default-rk3399-rockchip-suspe.patch"
+   "linux-libre-pinebook-pro-16-arm64-dts-rockchip-enable-earlycon.patch"
+   "linux-libre-pinebook-pro-17-arm64-dts-rockchip-reserve-memory-for-ATF-rockchip-S.patch"
+   "linux-libre-pinebook-pro-18-arm64-dts-rockchip-use-power-led-for-disk-activity-i.patch"
+   "linux-libre-pinebook-pro-20-arm64-dts-rockchip-add-typec-extcon-hack.patch"
+   "linux-libre-pinebook-pro-21-arm64-dts-rockchip-add-rockchip-suspend-node.patch"
+   "linux-libre-pinebook-pro-22-arm64-configs-add-defconfig-for-Pinebook-Pro.patch"
+   "linux-libre-pinebook-pro-23-arm64-dts-rockchip-setup-USB-type-c-port-as-dual-dat.patch"
+   "linux-libre-pinebook-pro-24-arm64-configs-Update-Pinbook-Pro-defconfig-to-v5.8-r.patch"
+   "linux-libre-pinebook-pro-25-soc-rockchip-Port-rockchip_pm_config-driver-to-Linux.patch"))
 
 (define (source-with-patches source patches)
   (origin
@@ -1082,9 +1080,13 @@ It has been modified to remove all non-free binary blobs.")
       (inherit base-linux-libre)
       (inputs `(("cpio" ,cpio) ,@(package-inputs base-linux-libre))))))
 
-(define-public linux-libre-pinebook-pro-5.8
-  (make-linux-libre* linux-libre-5.8-version
-                     linux-libre-5.8-source
+(define-public linux-libre-pinebook-pro
+  (make-linux-libre* linux-libre-5.10-version
+		     (source-with-patches linux-libre-5.10-pristine-source
+                                          (append
+                                           %pinebook-pro-patches
+                                           (list %boot-logo-patch
+                                                 %linux-libre-arm-export-__sync_icache_dcache-patch)))
                      '("aarch64-linux")
                      #:defconfig "pinebook_pro_defconfig"
                      #:extra-version "pinebook-pro"))
