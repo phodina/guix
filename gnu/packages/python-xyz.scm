56d3ee0a3f36968bb5d2bb4ef8b50a9ffc409910
Petr Hodina <phodina@protonmail.com>
2021-07-30 16:12:24 +0200

    gnu: Add python-emoji.
    * gnu/packages/python-xyz.scm (python-emoji): New variable.

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -17709,6 +17709,27 @@ simple, lightweight implementation.")
      "This library validates email address syntax and deliverability.")
     (license license:cc0)))
 
+(define-public python-emoji
+  (package
+    (name "python-emoji")
+    (version "0.6.0")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (pypi-uri "emoji" version))
+        (sha256
+          (base32
+            "04dx4f43j2zc22zdkgrs6wc6pv51hbv4dhhvd48fzy28svwa8bg4"))))
+    (build-system python-build-system)
+    (native-inputs
+      `(("python-coverage" ,python-coverage)
+        ("python-coveralls" ,python-coveralls)
+        ("python-pytest" ,python-pytest)))
+    (home-page "https://github.com/carpedm20/emoji/")
+    (synopsis "Emoji for Python")
+    (description "Emoji for Python")
+    (license license:bsd-3)))
+
 (define-public python-ukpostcodeparser
   (package
     (name "python-ukpostcodeparser")
