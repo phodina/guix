3ab5f1b2e83d1631704d537478fb1be57c5dba5e
Petr Hodina <phodina@protonmail.com>
2021-11-10 04:02:09 +0100

    gnu: Add rdate.
    * gnu/packages/time.scm (rdate): New variable.

--- gnu/packages/time.scm
+++ gnu/packages/time.scm
@@ -36,6 +36,7 @@
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (gnu packages time)
+  #:use-module (gnu packages autotools)
   #:use-module (gnu packages check)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages golang)
@@ -465,10 +466,34 @@ datetime type.")
     (synopsis "Python library for parsing ISO 8601 strings")
     (description
      "This package contains a library for parsing ISO 8601 datetime strings.")
-    (license bsd-3)))
+    (license license:bsd-3)))
 
 (define-public python2-aniso8601
   (package-with-python2 python-aniso8601))
+
+(define-public rdate
+  (let ((commit "91d84610e3695e90a884e2953908e95a856a9b74")
+        (revision "1"))
+    (package
+      (name "rdate")
+      (version commit)
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://github.com/njh/rdate")
+                      (commit version)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "002ryjq8gj1ml5la4v6dr3bh1zw5kxwy65rpziq8d2ccccarhv59"))))
+      (build-system gnu-build-system)
+      (native-inputs `(("autoconf" ,autoconf)
+                       ("automake" ,automake)))
+      (synopsis "Get date and time based on RFC 868")
+      (description "@code{rdate} connects to an RFC 868 time server over a TCP/IP
+network, printing the returned time and/or setting the system clock.")
+      (home-page "https://www.aelius.com/njh/rdate/")
+      (license license:gpl2))))
 
 (define-public datefudge
   (package
