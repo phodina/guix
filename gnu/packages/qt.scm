e5eec042cdbb2a002ab960e27c9f392d8fb61c1c
Petr Hodina <phodina@protonmail.com>
2021-11-11 12:49:58 +0100

    gnu: Add natron.
    * gnu/packages/qt.scm (natron): New variable.

--- gnu/packages/qt.scm
+++ gnu/packages/qt.scm
@@ -283,6 +283,42 @@ applications on Wayland.")
       (home-page "https://github.com/lirios/materialdecoration")
       (license license:lgpl3+))))
 
+(define-public natron
+  (package
+    (name "natron")
+    (version "2.4.0")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/NatronGitHub/Natron")
+                    (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "1vg8cj54x49kh7qfvhak6z41p436gw30jl9mxzb4fg114ifr3wk0"))))
+    (build-system qt-build-system)
+    (arguments
+     '(#:tests? #f                      ; no check target
+       #:make-flags
+       (list (string-append "PREFIX="
+                            (assoc-ref %outputs "out"))
+             (string-append "CXXFLAGS+=-fPIE -I" (assoc-ref
+                                        ; FIXME: Replace by variable package-version
+                                                  %build-inputs "python") "/include/python-3.8"))
+       #:phases
+       (modify-phases %standard-phases
+         (replace 'configure
+           (lambda* (#:key make-flags #:allow-other-keys)
+             (apply invoke (cons "qmake" make-flags)))))))
+    (native-inputs `(("python" ,python)
+                     ("pkg-config" ,pkg-config)))
+    (inputs `(("qtbase" ,qtbase-5)))
+    (synopsis "Node graph based video compositing software")
+    (description "Natron is a video compositor, similar in functionality
+to Adobe After Effects, Foundry's Nuke, or Blackmagic Fusion.")
+    (home-page "https://natrongithub.github.io/")
+    (license license:gpl2)))
+
 (define-public grantlee
   (package
     (name "grantlee")
