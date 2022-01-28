9e46cdf61b76fb99473264dbae1495939a4da03f
Petr Hodina <phodina@protonmail.com>
2022-01-28 10:49:05 +0100

    gnu: Add python-castagnait.
    * gnu/packages/kodi.scm (python-castagnait): New variable.

--- gnu/packages/kodi.scm
+++ gnu/packages/kodi.scm
@@ -83,6 +83,29 @@
   #:use-module (gnu packages xorg)
   #:use-module (gnu packages assembly))
 
+; https://pimylifeup.com/raspberry-pi-netflix/
+(define-public python-castagnait
+  (let ((commit "97c6ea586b006d1b56bc51a42c7f200af35f88e5")
+	(revision "1"))
+  (package
+    (name "python-castagnait")
+    (version commit)
+    (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+	       (url "https://github.com/castagnait/repository.castagnait")
+	       (commit version)))
+        (file-name (string-append name "-" version ".tar.gz"))
+        (sha256
+          (base32
+            "08z5cx0fwiqv01j0xxnh6y4ar1x981q0qpf2yz7hlz683as935b6"))))
+    (build-system python-build-system)
+    (home-page "https://github.com/castagnait/repository.castagnait")
+    (synopsis "Kodi Netflix addon")
+    (description "This package provides Kodi addon to enable Netflix.")
+    (license license:gpl2))))
+
 (define-public crossguid
   (let ((commit "fef89a4174a7bf8cd99fa9154864ce9e8e3bf989")
         (revision "2"))
