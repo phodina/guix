90e0652887cce6aa19a60b4615cd512ba6dae381
Petr Hodina <phodina@protonmail.com>
2022-08-05 14:13:41 +0200

    gnu: Add rust-radium-0.7.
    * gnu/packages/crates-io.scm (rust-radium-0.7): New variable.
    (rust-radium-0.7): Inherit from above.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -47655,8 +47655,30 @@ Racer.")
 fashion.  It is mostly used in Racer.")
     (license license:expat)))
 
+(define-public rust-radium-0.7
+  (package
+    (name "rust-radium")
+    (version "0.7.0")
+    (source (origin
+              (method url-fetch)
+              (uri (crate-uri "radium" version))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "02cxfi3ky3c4yhyqx9axqwhyaca804ws46nn4gc1imbk94nzycyw"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t))
+    (home-page "https://github.com/bitvecto-rs/radium")
+    (synopsis "Portable interfaces for maybe-atomic types")
+    (description
+     "@code{radium} provides abstractions and graceful degradation for behavior
+that must be shared-mutable, but merely may use atomic instructions to do so.")
+    (license license:expat)))
+
 (define-public rust-radium-0.6
   (package
+    (inherit rust-radium-0.7)
     (name "rust-radium")
     (version "0.6.2")
     (source
@@ -47671,13 +47693,7 @@ fashion.  It is mostly used in Racer.")
     (build-system cargo-build-system)
     (arguments
      `(#:cargo-development-inputs
-       (("rust-static-assertions" ,rust-static-assertions-1))))
-    (home-page "https://github.com/bitvecto-rs/radium")
-    (synopsis "Portable interfaces for maybe-atomic types")
-    (description
-     "@code{radium} provides abstractions and graceful degradation for behavior
-that must be shared-mutable, but merely may use atomic instructions to do so.")
-    (license license:expat)))
+       (("rust-static-assertions" ,rust-static-assertions-1))))))
 
 (define-public rust-radium-0.5
   (package
