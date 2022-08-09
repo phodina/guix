b2499fd9de9fe149fe328efcaef64b5392e4abb6
Petr Hodina <phodina@protonmail.com>
2022-08-10 01:18:23 +0200

    gnu: Add plasmatube.
    * gnu/packages/kde-plasma.scm (plasmatube): New variable.

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -42,6 +42,7 @@
   #:use-module (gnu packages linux)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages python)
+  #:use-module (gnu packages video)
   #:use-module (gnu packages qt)
   #:use-module (gnu packages xdisorg)
   #:use-module (gnu packages xorg)
@@ -651,3 +652,29 @@ Wayland.")
     (synopsis "Listen to traditional system notifications")
     (description "This package provides access to system notifications.")
     (license license:gpl2+)))
+
+(define-public plasmatube
+  (package
+    (name "plasmatube")
+    (version "22.09")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "mirror://kde/stable/plasma-mobile/"
+                                  version
+                                  "/"
+                                  name
+                                  "-"
+                                  version
+                                  ".tar.xz"))
+              (sha256
+               (base32
+                "00w9p5fcpv4s406lmcdcbrxf19sgkvf9yy8pfjmf1asvvvi8bpnk"))))
+    (build-system cmake-build-system)
+    (native-inputs (list extra-cmake-modules))
+    (inputs (list kirigami kconfig ki18n qtbase-5 qtdeclarative-5 qtquickcontrols2-5
+	qtsvg-5 qtmultimedia-5 youtube-dl))
+    (home-page "https://apps.kde.org/plasmatube/")
+    (synopsis "Kirigami YouTube video player")
+    (description "This package provides YouTube video player based
+on QtMultimedia and @command{yt-dlp}.")
+    (license license:gpl3+)))
