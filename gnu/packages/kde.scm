b2095982fe14c7f9e43393a454871e18a28be909
Petr Hodina <phodina@protonmail.com>
2022-09-11 20:56:58 +0200

    gnu: akregator: Remove kqtquickcharts form inputs.
    * gnu/packages/kde.scm (akregator) [inputs]: Remove kqtquickcharts. Add
    kquickcharts.

--- gnu/packages/kde.scm
+++ gnu/packages/kde.scm
@@ -194,7 +194,7 @@ This package contains GUI widgets for baloo.")
            kontactinterface
            kpimcommon
            kpimtextedit
-           kqtquickcharts
+           kquickcharts
            ktexteditor
            kuserfeedback
            libkdepim
