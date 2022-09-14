7ebdb74eee204cf35ad44076efc4457248c7f768
Petr Hodina <phodina@protonmail.com>
2022-09-14 21:24:49 +0200

    gnu: gthumb: Remove input labels and use gexp.
    * gnu/packages/gnome.scm (gthumb): Remove input labels and use gexp.

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -11690,38 +11690,38 @@ functionality.")
                 "09flm8s6jrvfya2ypw5873mnnani8ssy7wdv3ra1cljk4bjszy4p"))))
     (build-system meson-build-system)
     (arguments
-     `(#:glib-or-gtk? #t
+     (list #:glib-or-gtk? #t
        #:configure-flags
        ;; Ensure the RUNPATH contains all installed library locations.
-       (list (string-append "-Dc_link_args=-Wl,-rpath="
-                            (assoc-ref %outputs "out")
+       (list #~(string-append "-Dc_link_args=-Wl,-rpath="
+                            #$output
                             "/lib/gthumb/extensions")
              (string-append "-Dcpp_link_args=-Wl,-rpath="
-                            (assoc-ref %outputs "out")
+                            #$output
                             "/lib/gthumb/extensions"))))
     (native-inputs
-     `(("desktop-file-utils" ,desktop-file-utils) ; for update-desktop-database
-       ("glib:bin" ,glib "bin")                   ; for glib-compile-resources
-       ("gtk+:bin" ,gtk+ "bin")                   ; for gtk-update-icon-cache
-       ("intltool" ,intltool)
-       ("itstool" ,itstool)
-       ("pkg-config" ,pkg-config)
-       ("python" ,python)))
+     (list desktop-file-utils ; for update-desktop-database
+           `(,glib "bin")                   ; for glib-compile-resources
+           `(,gtk+ "bin")                   ; for gtk-update-icon-cache
+           intltool
+           itstool
+           pkg-config
+           python))
     (inputs
-     `(("clutter" ,clutter)
-       ("clutter-gst" ,clutter-gst)
-       ("clutter-gtk" ,clutter-gtk)
-       ("colord" ,colord)
-       ("exiv2" ,exiv2)
-       ("gsettings-desktop-schemas" ,gsettings-desktop-schemas)
-       ("gstreamer" ,gstreamer)
-       ("gtk" ,gtk+)
-       ("libheif" ,libheif)
-       ("libjpeg" ,libjpeg-turbo)
-       ("libraw" ,libraw)
-       ("librsvg" ,librsvg)
-       ("libtiff" ,libtiff)
-       ("libwebp" ,libwebp)))
+     (list clutter
+           clutter-gst
+           clutter-gtk
+           colord
+           exiv2
+           gsettings-desktop-schemas
+           gstreamer
+           gtk+
+           libheif
+           libjpeg-turbo
+           libraw
+           librsvg
+           libtiff
+           libwebp))
     (home-page "https://wiki.gnome.org/Apps/Gthumb")
     (synopsis "GNOME image viewer and browser")
     (description "GThumb is an image viewer, browser, organizer, editor and
