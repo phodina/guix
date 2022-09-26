610e07dfa6cb3dc434a7a29ab07661bf485390a7
Petr Hodina <phodina@protonmail.com>
2022-09-26 17:39:02 +0200

    gnu: Add print-manager.
    * gnu/packages/kde-utils.scm (print-manager): New variable.

--- gnu/packages/kde-utils.scm
+++ gnu/packages/kde-utils.scm
@@ -34,6 +34,7 @@
   #:use-module (gnu packages cmake)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages crypto)
+  #:use-module (gnu packages cups)
   #:use-module (gnu packages glib) ; dbus for tests
   #:use-module (gnu packages gnome)
   #:use-module (gnu packages gstreamer)
@@ -921,6 +922,28 @@ filter tool.  All modifications to the data loaded can be endlessly undone or
 redone.")
     (license ;; GPL for programs, LGPL for libraries, FDL for documentation
      (list license:gpl2+ license:lgpl2.0+ license:fdl1.2+))))
+
+(define-public print-manager
+  (package
+    (name "print-manager")
+    (version "22.08.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "mirror://kde/stable/release-service/" version
+                           "/src/print-manager-" version ".tar.xz"))
+       (sha256
+        (base32 "1abwj1ccm70adzx07ykqjlb4f6y55k2rv1j861dl0822187vl799"))))
+    (build-system qt-build-system)
+    (native-inputs
+     (list extra-cmake-modules))
+	(inputs (list cups kconfig kconfigwidgets kcoreaddons kdbusaddons
+  kiconthemes ki18n kcmutils kio knotifications plasma-framework kwidgetsaddons
+  kwindowsystem qtdeclarative-5))
+    (home-page "https://invent.kde.org/utilities/print-manager")
+    (synopsis "Tool for managing print jobs and printers")
+    (description "This package provides printing management for KDE.")
+    (license license:gpl2+)))
 
 (define-public rsibreak
   (package
