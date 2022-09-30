02aeb8a57ddc20845faa3c733b538094e9f6d01f
Petr Hodina <phodina@protonmail.com>
2022-09-30 22:09:02 +0200

    gnu: Add kde-cli-tools.
    * gnu/packages/kde-plasma.scm (kde-cli-tools): New variable.
    * gnu/local.mk: Add patch.
    * gnu/packages/patches/kde-cli-tools-Add-delay-to-update-the-mime-db-in-testCreateMimeTyp.patch:
    New file.

--- gnu/local.mk
+++ gnu/local.mk
@@ -1368,6 +1368,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/lightdm-vnc-color-depth.patch		\
   %D%/packages/patches/localed-xorg-keyboard.patch		\
   %D%/packages/patches/kcontacts-Fix-incorrect-name-of-country-in-compariso.patch \
+  %D%/packages/patches/kde-cli-tools-Add-delay-to-update-the-mime-db-in-testCreateMimeTyp.patch	\
   %D%/packages/patches/kdiagram-Fix-missing-link-libraries.patch \
   %D%/packages/patches/kiki-level-selection-crash.patch		\
   %D%/packages/patches/kiki-makefile.patch			\
