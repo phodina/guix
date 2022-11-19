03fadd5b8cf147a27e3df5244ea9b9aec1705e1d
Petr Hodina <phodina@protonmail.com>
2022-11-19 21:47:12 +0100

    gnu: Add rust-rkyv-0.7.
    * gnu/packages/crates-io.scm (rust-rkyv-0.7): New variable.
    (rust-rkyv-0.6): Remove variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -48673,25 +48673,27 @@ sub-processes using a fork-like interface.")
         (base32
          "1n3iw9kaq70dw1rvvma0gjwydbj0f2mvvqvrva69f5cl6yv1dnd0"))))))
 
-(define-public rust-rkyv-0.6
+(define-public rust-rkyv-0.7
   (package
     (name "rust-rkyv")
-    (version "0.6.7")
+    (version "0.7.39")
     (source
      (origin
        (method url-fetch)
        (uri (crate-uri "rkyv" version))
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
-        (base32 "01wsn0i8gsw958j892w8i4fyzjdsyhrh7c5zajig049kbqz5n4yb"))))
+        (base32 "05gdspzw03hq6l58si4ixfj5xd27ljw6fiqksggnvn87bd4b7hnf"))))
     (build-system cargo-build-system)
     (arguments
      `(#:skip-build? #t
        #:cargo-inputs
-       (("rust-bytecheck" ,rust-bytecheck-0.5)
-        ("rust-memoffset" ,rust-memoffset-0.6)
+       (("rust-bytecheck" ,rust-bytecheck-0.6)
+        ("rust-hashbrown" ,rust-hashbrown-0.12)
+        ("rust-indexmap" ,rust-indexmap-1)
         ("rust-ptr-meta" ,rust-ptr-meta-0.1)
-        ("rust-rkyv-derive" ,rust-rkyv-derive-0.6)
+        ("rust-rend" ,rust-rend-0.3)
+        ("rust-rkyv-derive" ,rust-rkyv-derive-0.7)
         ("rust-seahash" ,rust-seahash-4))))
     (home-page "https://github.com/rkyv/rkyv")
     (synopsis "Zero-copy deserialization framework for Rust")
