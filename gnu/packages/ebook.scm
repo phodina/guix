0336189e02cacf1a956093303ac081cd3707ec2e
Petr Hodina <phodina@protonmail.com>
2022-12-03 09:47:06 +0100

    gnu: Add wormbook.
    * gnu/packages/ebook.scm (wormbook): New variable.

--- gnu/packages/ebook.scm
+++ gnu/packages/ebook.scm
@@ -81,7 +81,48 @@
   #:use-module (gnu packages web)
   #:use-module (gnu packages xml)
   #:use-module (gnu packages xorg)
+  #:use-module (gnu packages webkit)
   #:use-module (gnu packages wxwidgets))
+
+(define-public bookworm
+ (package
+   (name "bookworm")
+   (version "1.1.2")
+   (source (origin
+             (method git-fetch)
+             (uri (git-reference
+              (url "https://github.com/babluboy/bookworm")
+              (commit version)))
+             (file-name (git-file-name name version))
+             (sha256
+              (base32
+               "0w0rlyahpgx0l6inkbj106agbnr2czil0vdcy1zzv70apnjz488j"))))
+   (build-system meson-build-system)
+   (arguments
+     (list #:phases
+       #~(modify-phases %standard-phases
+          (add-after 'install 'fix-project-name
+            (lambda* _
+              (invoke "mv" (string-append #$output "/bin/com.github.babluboy.bookworm")
+                     (string-append #$output "/bin/bookworm")))))))
+   (native-inputs (list `(,glib "bin")
+;                    cmake
+                    desktop-file-utils
+                    pkg-config))
+   (inputs (list python
+                 libgee
+                 webkitgtk
+                 gtk+
+                 granite
+                 poppler
+                 vala))
+  (synopsis "Simple ebook reader")
+  (description "Read the books you love without having to worry about
+the different format complexities like epub, pdf, mobi, cbr, etc.
+This version supports EPUB, MOBI, FB2, PDF, FB2 and Comics (CBR and CBZ)
+formats.")
+  (home-page "https://github.com/babluboy/bookworm")
+  (license license:gpl3+)))
 
 (define-public chmlib
   (package
