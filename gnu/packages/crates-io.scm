ab3df40af3d4fb06859ac6d73b0a7e0cccf4e2d8
Petr Hodina <phodina@protonmail.com>
2022-12-06 10:21:18 +0100

    gnu: Add rust-shadow-rs-0.6.
    * gnu/packages/crates.io (rust-shadow-rs-0.6): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -52587,20 +52587,19 @@ functions core functionality.")
   (package
     (name "rust-shadow-rs")
     (version "0.8.1")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "shadow-rs" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "0iy4f6yzag52qr93hjhm2l8crxhjblr91nffbdd8zzw0xchfjdc9"))))
+    (source (origin
+              (method url-fetch)
+              (uri (crate-uri "shadow-rs" version))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "0iy4f6yzag52qr93hjhm2l8crxhjblr91nffbdd8zzw0xchfjdc9"))))
     (build-system cargo-build-system)
     (arguments
      `(#:skip-build? #t
-       #:cargo-inputs
-       (("rust-chrono" ,rust-chrono-0.4)
-        ("rust-git2" ,rust-git2-0.13)
-        ("rust-is-debug" ,rust-is-debug-1))))
+       #:cargo-inputs (("rust-chrono" ,rust-chrono-0.4)
+                       ("rust-git2" ,rust-git2-0.13)
+                       ("rust-is-debug" ,rust-is-debug-1))))
     (home-page "https://github.com/baoyachi/shadow-rs")
     (synopsis "Recall properties of the build process at run time")
     (description
@@ -52608,6 +52607,28 @@ functions core functionality.")
 and environment at run time.  You can use this tool to check in production
 exactly where a binary came from and how it was built.")
     (license (list license:expat license:asl2.0))))
+
+(define-public rust-shadow-rs-0.6
+  (package
+    (inherit rust-shadow-rs-0.8)
+    (name "rust-shadow-rs")
+    (version "0.6.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "shadow-rs" version))
+       (file-name
+        (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32
+         "0sps996yhchz43zfk9zysjpirz638s1hbhspp998bk6v9l6k7j10"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build?
+       #t
+       #:cargo-inputs
+       (("rust-chrono" ,rust-chrono-0.4)
+        ("rust-git2" ,rust-git2-0.13))))))
 
 (define-public rust-sharded-slab-0.1
   (package
