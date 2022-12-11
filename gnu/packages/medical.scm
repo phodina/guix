960dfac430fdb7315d344b8bae60669712d9f43a
Petr Hodina <phodina@protonmail.com>
2022-12-11 18:10:17 +0100

    gnu: Add mygnuhealth.
    * gnu/packages/medical.scm (mygnuhealth): New variable.

--- gnu/packages/medical.scm
+++ gnu/packages/medical.scm
@@ -1,6 +1,7 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2017 Ricardo Wurmus <rekado@elephly.net>
 ;;; Copyright © 2017 Quiliro <quiliro@fsfla.org>
+;;; Copyright © 2022 Petr Hodina <phodina@protonmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -20,11 +21,65 @@
 (define-module (gnu packages medical)
   #:use-module (guix build-system python)
   #:use-module (guix download)
+  #:use-module (guix gexp)
   #:use-module (guix licenses)
   #:use-module (guix packages)
   #:use-module (gnu packages databases)
+  #:use-module (gnu packages kde-frameworks) ; kirigami
+  #:use-module (gnu packages python-crypto)
+  #:use-module (gnu packages python-web)
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages qt))
+
+(define-public mygnuhealth
+  (package
+    (name "mygnuhealth")
+    (version "1.0.5")
+    (source (origin
+              (method url-fetch)
+              (uri (pypi-uri "MyGNUHealth" version))
+              (sha256
+               (base32
+                "1jcrriccqzb4jx7zayhiqmpvi3cvfy3bbf9zr3m83878f94yww8j"))))
+    (build-system python-build-system)
+    (arguments
+     (list #:phases #~(modify-phases %standard-phases
+                        (add-after 'install 'wrap-executable
+                          (lambda* (#:key inputs #:allow-other-keys)
+                            (let ((out #$output)
+                                  (qml "/lib/qt5/qml"))
+                              (wrap-program (string-append out
+                                                           "/bin/mygnuhealth")
+                                            `("QML2_IMPORT_PATH" ":" prefix
+                                              (,(string-append out qml)
+                                               ,@(map (lambda (i)
+                                                     (string-append
+                                                      (assoc-ref inputs i) qml))
+                                               '("kirigami"
+                                                 "qtdeclarative"
+                                                 "qtgraphicaleffects"
+                                                 "qtquickcontrols"
+                                                 "qtquickcontrols2"))))))))
+                        (add-before 'check 'env-setup
+                          (lambda _
+                            (mkdir-p "/tmp/mygh/")
+                            (setenv "HOME" "/tmp"))))))
+    (native-inputs (list python-pyside-2))
+    (inputs (list qtbase-5
+                  qtdeclarative-5
+                  qtgraphicaleffects
+                  qtquickcontrols-5
+                  qtquickcontrols2-5
+                  kirigami))
+    (propagated-inputs (list python-bcrypt python-matplotlib python-requests
+                             python-tinydb))
+    (home-page "https://www.gnuhealth.org")
+    (synopsis "The GNU Health Personal Health Record")
+    (description
+     "This package provides GNUHealth Personal Health Record
+application for desktop and mobile devices that integrates with the GNU
+Health Federation.")
+    (license gpl3+)))
 
 (define-public openmolar-1
   (package
